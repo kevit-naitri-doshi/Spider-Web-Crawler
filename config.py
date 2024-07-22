ad2c15ffcac58745de4287eebf7bf4f4fafea711
kevit-naitri-doshi <naitri.doshi@kevit.io>
2024-07-22 11:05:21 +0530

    Spider Web Crawling Complete

---
+++ config.py
@@ -0,0 +1,9 @@
+from domain import *
+
+PROJECT_NAME='realpython'
+HOMEPAGE= 'https://realpython.com/'
+DOMAIN_NAME=get_domain_name(HOMEPAGE)
+QUEUE_FILE=PROJECT_NAME + '/queue.txt'
+CRAWLED_FILE=PROJECT_NAME + '/crawled.txt'
+NUMBER_OF_THREADS=8
+
---
+++ general.py
@@ -0,0 +1,47 @@
+import os
+
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print("Creating Directory "+directory)
+        os.makedirs(directory)
+
+def create_data_files(project_name,base_url):
+    queue=project_name+'/queue.txt'
+    crawled=project_name + '/crawled.txt'
+
+    if not os.path.isfile(queue):
+        write_file(queue,base_url)
+
+    
+    if not os.path.isfile(crawled):
+        write_file(crawled,'')
+
+
+def write_file(path,data):
+    with open(path, 'w') as f:
+        f.write(data)
+
+def append_to_file(path,data):
+    with open(path,'a') as file:
+        file.write(data+'\n')
+
+def delete_file_contents(path):
+    open(path,'w').close()
+
+
+def file_to_set(filename):
+    results=set()
+    with open(filename,'rt') as file:
+        for line in file:
+            results.add(line.replace('\n',''))
+    return results
+
+
+def set_to_file(links,file):
+    # delete_file_contents(file)
+    with open(file,'w') as f:
+        for link in sorted(links):
+            f.write(link+'\n')
+
+
+
